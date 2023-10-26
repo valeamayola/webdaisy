71013904a9776598b398857ae559b7756eb47efe
valeamayola <valee.amayola@gmail.com>
2023-10-26 17:32:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-success rounded-box">
+  <div className="carousel-item">
+    <img src='../src/img/img1.jpg' className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="../src/img/img2.jpg" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="../src/img/img3.jpg" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="../src/img/img4.jpg" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="../src/img/img5.jpg" className="rounded-box" />
+  </div> 
+  <div className="carousel-item">
+    <img src="../src/img/img6.jpg" className="rounded-box" />
+  </div> 
+</div>
+  );
+}
+
+export default App;
