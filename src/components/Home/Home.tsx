44fe5638656af3f0db2a015fc1727f95d0e68421
valeamayola <valee.amayola@gmail.com>
2023-11-21 17:27:47 -0300

    add example button for view markers

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,14 @@
 import { NavBar } from "./NavBar";
 import { Menu } from "./Menu/Menu";
 import { menuItems } from "./Menu/menuItems";
+import { useNavigate } from "react-router-dom";
 
 export function Home () {
+  const navigate = useNavigate ();
   return (
     <div>
       <NavBar />
+      <button className="btn" onClick={() => navigate('/markers')}>MARCADOR EJEMPLO</button>
       <Menu items={menuItems}/>
     </div>
   );
