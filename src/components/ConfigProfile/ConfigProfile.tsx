ece49ccd5a13c6173c11c715e32373c1eb25df86
valeamayola <valee.amayola@gmail.com>
2023-11-21 15:27:17 -0300

    add configprofile

--- src/components/ConfigProfile/ConfigProfile.tsx
+++ src/components/ConfigProfile/ConfigProfile.tsx
@@ -1,8 +1,10 @@
 import '../../App.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export function ConfigProfile () {
     const navigate = useNavigate ();
+    const [checked, setChecked] = useState(true);
     return (
         <div>
             <div className='absolute top-5 left-0 ml-5'>
@@ -12,13 +14,21 @@ export function ConfigProfile () {
         </div>
         <div className='flex flex-col'>
             <span className='mb-5 font-medium text-xl'>CONFIGURACIÓN</span>
-            <span className='text-start font-medium'>General</span>
-            <div className='m-5 bg-neutral-200 rounded-xl'>
-            
+            <div className='form-control p-4 mt-2 mb-5 bg-neutral-200 rounded-xl'>
+                <label className='label cursor-pointer'>
+                <span className='mr-10 label-text font-normal text-base'>Activar notificaciones</span>
+                <input type="checkbox" checked={checked} onChange={(event) => {setChecked(event.target.checked);}} className="toggle rounded-3xl"/>
+                </label>
             </div>
-            <button className='btn btn-wide mb-5'>Cambiar contraseña</button>
-            <button className='btn btn-wide'></button>
+            <div className='p-4 mt-0 mb-5 bg-neutral-200 rounded-xl'>
+                <span className='mr-10 label-text font-normal text-base'>Cambiar contraseña</span>
+                <button className="btn btn-sm ml-6 bg-transparent border-transparent" onClick={() => navigate('')}>
+                    <svg className='w-2' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/>
+                        </svg>
+                </button>
+            </div>
         </div>
-        </div>
+        </div> 
     )
 }
